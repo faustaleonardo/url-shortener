713241e37cba25bf7b50d5bfd2e934479a4e6e74
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-04-29 19:59:13 +0700

    ready for production

--- index.js
+++ index.js
@@ -29,14 +29,14 @@ app.use(passport.session());
 app.use('/api/auth', authRoutes);
 app.use('/api/', urlRoutes);
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-//   const path = require('path');
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 sequelize.sync().then(() => {
