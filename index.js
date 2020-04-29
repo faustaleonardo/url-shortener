f560e47bfde569c502df8cba0b47e37cea22c84b
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-04-29 19:55:12 +0700

    serve js file

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
 require('./services/passport');
 
-const port = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json());
@@ -31,16 +29,17 @@ app.use(passport.session());
 app.use('/api/auth', authRoutes);
 app.use('/api/', urlRoutes);
 
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+const port = process.env.PORT || 5000;
 sequelize.sync().then(() => {
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-
   app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
   });
